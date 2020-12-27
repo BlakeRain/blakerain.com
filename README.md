ccb60e1bd41560b3c0d05c1276a8578bb823ae36
Blake Rain <blakerain@blakes-macbook.home>
2020-12-27 23:19:41 +0000

    Add search lambda

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # blakerain.com
-Repository for the Ghost theme of my blog
+
+Repository for the customized [Casper](https://github.com/tryghost/Casper) theme for my blog
+along with various related bits and pieces.
+
+## Search Data Workflow
+
+This repository contains the Python
+[script](https://github.com/BlakeRain/blakerain.com/blob/master/search/update-search.py) which
+updates the search data used by the site. This script is run by a GitHub workflow called
+[Update Search Data](https://github.com/BlakeRain/blakerain.com/blob/master/.github/workflows/update-search-data.yml).
+This workflow is triggered either manually or when I update or publish a post on the site. A set
+of webhooks are installed in Ghost which call an AWS Lambda function called
+[ghost-post-actions](https://github.com/BlakeRain/blakerain.com/blob/master/lambda/ghost-post-actions/index.js).
+This translates the Ghost webhook to an invocation of the GitHub workflow via the GitHub API.
---
+++ lambda/ghost-post-actions/index.js
@@ -0,0 +1,43 @@
+const http = require("https");
+
+exports.handler = async (event) => {
+  const req = await new Promise((resolve, reject) => {
+    const req = http.request(
+      {
+        host: "api.github.com",
+        method: "POST",
+        port: 443,
+        path: "/repos/blakerain/blakerain.com/dispatches",
+        headers: {
+          Authorization: "token " + process.env.GITHUB_TOKEN,
+          Accept: "application/vnd.github.v3+json",
+          "User-Agent": "BlakeRain",
+        },
+      },
+      (res) => {
+        let data = "";
+
+        res.setEncoding("utf8");
+        res.on("data", (chunk) => (data += chunk));
+        res.on("end", () => {
+          resolve({
+            statusCode: res.statusCode,
+            body: data,
+          });
+        });
+      }
+    );
+
+    req.on("error", (e) =>
+      reject({
+        statusCode: 500,
+        body: e,
+      })
+    );
+
+    req.write('{"event_type":"event_type"}');
+    req.end();
+  });
+
+  return req;
+};
