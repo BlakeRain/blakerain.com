182d04fd291f961573985bf5d9b588656b8ace05
Blake Rain <blake.rain@blakerain.com>
2021-07-31 21:32:21 +0100

    Add commento

--- src/containers/BlogPost.js
+++ src/containers/BlogPost.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useRouteData, Head } from "react-static";
 import Content from "../components/Content";
 
 export default function BlogPost() {
+  const commento = useRef();
   const { authors, tags, post } = useRouteData();
+
+  useEffect(() => {
+    const commento_script = document.createElement("SCRIPT");
+    commento_script.defer = true;
+    commento_script.src = "https://cdn.commento.io/js/commento.js";
+    commento.current.parentNode.appendChild(commento_script);
+  });
 
   return (
     <div>
@@ -11,6 +19,9 @@ export default function BlogPost() {
         <title>{post.title}</title>
       </Head>
       <Content authors={authors} tags={tags} content={post} />
+      <section className="post-comments">
+        <div ref={commento} id="commento"></div>
+      </section>
     </div>
   );
 }
