db8bda16d211291559651782c32cbaf9de8bdd78
Blake Rain <blake.rain@blakerain.com>
2021-08-06 16:10:09 +0100

    Use 24-hour time in footer (rather than 1y)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
           </a>
           <a href="https://status.blakerain.com/" title="Status page">
             <img
-              src="https://img.shields.io/endpoint?url=https%3A%2F%2Fraw.githubusercontent.com%2FBlakeRain%2Fupptime.blakerain.com%2Fmaster%2Fapi%2Fblakerain-com%2Fresponse-time.json"
+              src="https://img.shields.io/endpoint?url=https%3A%2F%2Fraw.githubusercontent.com%2FBlakeRain%2Fupptime.blakerain.com%2Fmaster%2Fapi%2Fblakerain-com%2Fresponse-time-week-day.json"
               alt="Website response time metric"
             />
           </a>
