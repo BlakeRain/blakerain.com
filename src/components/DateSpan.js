7e37aded3f51a5895c5d53ce964a1d6853f5ad34
Blake Rain <blake.rain@blakerain.com>
2021-08-16 16:08:49 +0100

    Tidy up date component

--- src/components/DateSpan.js
+++ src/components/DateSpan.js
@@ -1,34 +1,22 @@
 import React from "react";
 
+const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const monthName = (month) => {
-  switch (month) {
-    case 0:
-      return "January";
-    case 1:
-      return "February";
-    case 2:
-      return "March";
-    case 3:
-      return "April";
-    case 4:
-      return "May";
-    case 5:
-      return "June";
-    case 6:
-      return "July";
-    case 7:
-      return "August";
-    case 8:
-      return "September";
-    case 9:
-      return "October";
-    case 10:
-      return "November";
-    case 11:
-      return "December";
-    default:
-      return "???";
-  }
+  return month >= 0 && month < 12 ? MONTH_NAMES[month] : "???";
 };
 
 const DateSpan = ({ date }) => (
