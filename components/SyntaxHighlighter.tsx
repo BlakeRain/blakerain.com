65d9c75d225fcd2afa16a82234830cf0a63d272c
Blake Rain <blake.rain@blakerain.com>
2021-08-18 18:29:51 +0100

    Add languages to Syntax highlighter

--- components/SyntaxHighlighter.tsx
+++ components/SyntaxHighlighter.tsx
@@ -1,38 +1,41 @@
 import { FC, useEffect, useRef } from "react";
-import { Prism as SyntaxHighlighten } from "react-syntax-highlighter";
+import Prism from "prismjs";
+
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-nginx";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
 
 const SyntaxHighlighter: FC = (props) => {
-  // const contentRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
-  //   var highlight_count = 0;
-  //   const highlight_start = performance.now();
+  useEffect(() => {
+    var highlight_count = 0;
+    const highlight_start = performance.now();
 
-  //   if (!contentRef.current) {
-  //     return;
-  //   }
+    if (!contentRef.current) {
+      return;
+    }
 
-  //   Prism.highlightAll(false, (element) => {
-  //     console.log("Highlighting", element);
-  //   });
+    contentRef.current.querySelectorAll('code[class*="language-"]').forEach((element) => {
+      Prism.highlightElement(element, false, () => {
+        ++highlight_count;
+      });
+    });
 
-  // contentRef.current.querySelectorAll('code[class*="language-"]').forEach((element) => {
-  //   Prism.highlightElement(element, false, () => {
-  //     ++highlight_count;
-  //   });
-  // });
+    if (highlight_count > 0) {
+      console.log(
+        `Completed syntax highlighting of ${highlight_count} element(s) in ${(
+          performance.now() - highlight_start
+        ).toFixed(2)} ms`
+      );
+    }
+  }, []);
 
-  // if (highlight_count > 0) {
-  //   console.log(
-  //     `Completed syntax highlighting of ${highlight_count} element(s) in ${(
-  //       performance.now() - highlight_start
-  //     ).toFixed(2)} ms`
-  //   );
-  // }
-  // }, []);
-
-  // return <div ref={contentRef}>{props.children}</div>;
-  return <SyntaxHighlighten>{props.children}</SyntaxHighlighten>;
+  return <div ref={contentRef}>{props.children}</div>;
 };
 
 export default SyntaxHighlighter;
