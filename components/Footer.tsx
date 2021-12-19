c601dd394bb5819fa92ac64d3732377e09d82a23
Blake Rain <blake.rain@blakerain.com>
2021-12-19 12:17:59 +0000

    Remove SA link and add link to analytics page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,8 @@ export const Footer: FC = () => {
             href="https://status.blakerain.com/"
             title="Status page"
             referrerPolicy="origin"
-            rel="noopener">
+            rel="noopener"
+          >
             <img
               src="https://img.shields.io/endpoint?url=https%3A%2F%2Fraw.githubusercontent.com%2FBlakeRain%2Fupptime.blakerain.com%2Fmaster%2Fapi%2Fblakerain-com%2Fuptime.json"
               alt="Website uptime metric"
@@ -29,7 +30,8 @@ export const Footer: FC = () => {
             href="https://status.blakerain.com/"
             title="Status page"
             referrerPolicy="origin"
-            rel="noopener">
+            rel="noopener"
+          >
             <img
               src="https://img.shields.io/endpoint?url=https%3A%2F%2Fraw.githubusercontent.com%2FBlakeRain%2Fupptime.blakerain.com%2Fmaster%2Fapi%2Fblakerain-com%2Fresponse-time-day.json"
               alt="Website response time metric"
@@ -47,23 +49,12 @@ export const Footer: FC = () => {
             <Link href="/disclaimer">
               <a>Disclaimer</a>
             </Link>
+            <Link href="/analytics">
+              <a>Analytics</a>
+            </Link>
             <a href="https://twitter.com/HalfWayMan">Twitter</a>
             <a href="https://ghost.org/">Ghost CMS</a>
           </nav>
-          <a
-            className={styles.simpleAnalytics}
-            href="https://simpleanalytics.com/?utm_source=blakerain.com&utm_content=badge"
-            referrerPolicy="origin"
-            rel="noreferrer"
-            target="_blank">
-            <img
-              src="https://simpleanalyticsbadge.com/blakerain.com?mode=dark&background=1a1c20&logo=13304e"
-              loading="lazy"
-              referrerPolicy="no-referrer"
-              crossOrigin="anonymous"
-              alt="Simple Analytics"
-            />
-          </a>
         </section>
       </div>
     </footer>
