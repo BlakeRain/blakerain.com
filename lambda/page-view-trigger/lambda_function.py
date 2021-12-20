5c17ef869596797b47228da2daf0df02e81678e5
Blake Rain <blake.rain@blakerain.com>
2021-12-20 22:27:50 +0000

    Added user-agent scanning and browser stats

--- lambda/page-view-trigger/lambda_function.py
+++ lambda/page-view-trigger/lambda_function.py
@@ -1,7 +1,10 @@
+import dependencies
+
 from datetime import datetime
 
 import boto3
 import os
+import user_agents
 
 DDB = boto3.client("dynamodb")
 DDB_TABLE_NAME = os.getenv("DDB_TABLE_NAME", "PageTable")
@@ -15,8 +18,8 @@ def update_path(path: str, time: datetime):
     },
         UpdateExpression="SET ViewDay = :d, ViewHour = :h ADD ViewCount :n",
         ExpressionAttributeValues={
-        ":d": {"S": f"{time.strftime('%Y-%m-%d')}"},
-        ":h": {"S": f"{time.hour}"},
+        ":d": {"S": time.strftime('%Y-%m-%d')},
+        ":h": {"S": str(time.hour)},
         ":n": {"N": "1"}
     })
 
@@ -27,8 +30,8 @@ def update_path(path: str, time: datetime):
     },
         UpdateExpression="SET ViewWeek = :w, ViewDay = :d ADD ViewCount :n",
         ExpressionAttributeValues={
-        ":w": {"S": f"{time.strftime('%Y-%W')}"},
-        ":d": {"S": f"{time.strftime('%w')}"},
+        ":w": {"S": time.strftime('%Y-%W')},
+        ":d": {"S": time.strftime('%w')},
         ":n": {"N": "1"}
     })
 
@@ -39,8 +42,34 @@ def update_path(path: str, time: datetime):
     },
         UpdateExpression="SET ViewMonth = :m, ViewDay = :d ADD ViewCount :n",
         ExpressionAttributeValues={
-        ":m": {"S": f"{time.strftime('%Y-%m')}"},
-        ":d": {"S": f"{time.day}"},
+        ":m": {"S": time.strftime('%Y-%m')},
+        ":d": {"S": str(time.day)},
+        ":n": {"N": "1"}
+    })
+
+
+def update_user_agent(time: datetime, user_agent_string: str):
+    ua = user_agents.parse(user_agent_string)
+
+    DDB.update_item(TableName=DDB_TABLE_NAME, Key={
+        "Path": {"S": "browser"},
+        "Section": {"S": f"Week-{time.strftime('%Y-%W-%w')}#{ua.browser.family.replace(' ', '-')}"}
+    },
+        UpdateExpression="SET ViewWeek = :w, ViewDay = :d ADD ViewCount :n",
+        ExpressionAttributeValues={
+        ":w": {"S": time.strftime("%Y-%W")},
+        ":d": {"S": time.strftime("%w")},
+        ":n": {"N": "1"}
+    })
+
+    DDB.update_item(TableName=DDB_TABLE_NAME, Key={
+        "Path": {"S": "browser"},
+        "Section": {"S": f"Month-{time.strftime('%Y-%m-%d')}#{ua.browser.family.replace(' ', '-')}"}
+    },
+        UpdateExpression="SET ViewMonth = :m, ViewDay = :d ADD ViewCount :n",
+        ExpressionAttributeValues={
+        ":m": {"S": time.strftime("%Y-%m")},
+        ":d": {"S": str(time.day)},
         ":n": {"N": "1"}
     })
 
@@ -71,3 +100,4 @@ def lambda_handler(event, context):
                     time = datetime.fromisoformat(image["Time"]["S"])
                     update_path(path, time)
                     update_path("site", time)
+                    update_user_agent(time, image["UserAgent"]["S"])
