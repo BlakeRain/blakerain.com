8754b691897566f932cab6fbc7b9aff9d37a34fc
Blake Rain <blake.rain@blakerain.com>
2021-12-24 15:52:35 +0000

    Add pie-chart for browser type

--- components/analytics/BrowserReport.tsx
+++ components/analytics/BrowserReport.tsx
@@ -49,6 +49,52 @@ export const BrowserReport: FC<{
       return obj;
     });
 
+  const N = 8;
+  const topN: {
+    names: [string];
+    total: number;
+    percent: number;
+    start: number;
+    color: string;
+  }[] = [];
+  for (let i = 0; i < browsers.length; ++i) {
+    if (i < N - 1) {
+      topN.push({
+        names: [browsers[i].name],
+        total: browsers[i].total,
+        percent: 0,
+        start: 0,
+        color: browsers[i].color,
+      });
+    } else if (i === N - 1) {
+      topN.push({
+        names: [browsers[i].name],
+        total: browsers[i].total,
+        percent: 0,
+        start: 0,
+        color: "#444",
+      });
+    } else {
+      topN[topN.length - 1].names.push(browsers[i].name);
+      topN[topN.length - 1].total += browsers[i].total;
+    }
+  }
+
+  topN.sort((a, b) => (a.total = b.total));
+  const topN_total = topN.reduce((total, item) => total + item.total, 0);
+  var last_percent = 0;
+  topN.forEach((item) => {
+    item.start = last_percent;
+    item.percent = item.total / topN_total;
+    last_percent = item.start + item.percent;
+  });
+
+  const getCoordinatesForPercent = (percent: number): [number, number] => {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+    return [x, y];
+  };
+
   return (
     <React.Fragment>
       <div>
@@ -69,26 +115,60 @@ export const BrowserReport: FC<{
           onMouseOut={() => setHighlight(null)}
         />
       </div>
-      <div className={styles.browserPalette}>
-        {browsers.map((browser, index) => (
-          <div
-            className={
-              highlight === null || highlight === browser.name
-                ? ""
-                : styles.inactive
-            }
-            key={index.toString()}
-            style={{ backgroundColor: browser.color }}
-            onMouseOver={() => setHighlight(browser.name)}
-            onMouseOut={() => setHighlight(null)}
-          >
-            <div>
-              <BrowserIcon name={browser.name} />
-              {browser.name}
+      <div className={styles.browserInfo}>
+        <div>
+          <svg viewBox="-1 -1 2 2" style={{ transform: "rotate(-0.25turn)" }}>
+            {topN.map((browser, index) => {
+              const [sx, sy] = getCoordinatesForPercent(browser.start);
+              const [ex, ey] = getCoordinatesForPercent(
+                browser.start + browser.percent
+              );
+              const large_arc = browser.percent > 0.5 ? 1 : 0;
+              const path = [
+                `M ${sx} ${sy}`,
+                `A 1 1 0 ${large_arc} 1 ${ex} ${ey}`,
+                `L 0 0`,
+              ].join(" ");
+
+              return (
+                <path
+                  key={index.toString()}
+                  fill={browser.color}
+                  d={path}
+                  fillOpacity={
+                    highlight === null ||
+                    browser.names.indexOf(highlight) !== -1
+                      ? "1.0"
+                      : "0.1"
+                  }
+                />
+              );
+            })}
+          </svg>
+        </div>
+        <div className={styles.browserPalette}>
+          {browsers.map((browser, index) => (
+            <div
+              className={
+                styles.browserButton +
+                " " +
+                (highlight === null || highlight === browser.name
+                  ? ""
+                  : styles.inactive)
+              }
+              key={index.toString()}
+              style={{ backgroundColor: browser.color }}
+              onMouseOver={() => setHighlight(browser.name)}
+              onMouseOut={() => setHighlight(null)}
+            >
+              <div>
+                <BrowserIcon name={browser.name} />
+                {browser.name}
+              </div>
+              <b>{browser.total}</b>
             </div>
-            <b>{browser.total}</b>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </React.Fragment>
   );
