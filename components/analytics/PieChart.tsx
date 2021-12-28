3549a42446104646bd9ac1ec37d2e6eaae8ef23f
Blake Rain <blake.rain@blakerain.com>
2021-12-28 15:01:35 +0000

    Add memoization of PieChart path calculations

--- components/analytics/PieChart.tsx
+++ components/analytics/PieChart.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 
 export interface PieChartPoint {
   label: string;
@@ -9,51 +9,57 @@ export interface PieChartPoint {
 export const PieChart: FC<{
   data: PieChartPoint[];
   highlight?: string;
-  onHighlight?: (name: string | null) => void;
+  onHighlight?: (name: string | null, index: number) => void;
 }> = ({ data, highlight, onHighlight }) => {
   const coord = (ratio: number): [number, number] => {
     const t = 2 * Math.PI * ratio;
     return [Math.cos(t), Math.sin(t)];
   };
 
-  const paths = [];
-  for (let last = 0, i = 0; i < data.length; last = last + data[i].ratio, ++i) {
-    const [sx, sy] = coord(last);
-    const [ex, ey] = coord(last + data[i].ratio);
-    const large = data[i].ratio > 0.5 ? 1 : 0;
-    const path = [
-      `M ${sx} ${sy}`,
-      `A 1 1 0 ${large} 1 ${ex} ${ey}`,
-      `L 0 0`,
-    ].join(" ");
+  const paths = useMemo(() => {
+    const paths = [];
 
-    paths.push(
-      <path
-        key={i.toString()}
-        d={path}
-        fill={data[i].color}
-        fillOpacity={
-          typeof highlight !== "string" || data[i].label === highlight
-            ? "1.0"
-            : "0.1"
-        }
-        onMouseOver={() => {
-          if (onHighlight) {
-            onHighlight(data[i].label);
-          }
-        }}
-        onMouseOut={() => {
-          if (onHighlight) {
-            onHighlight(null);
-          }
-        }}
-      />
-    );
-  }
+    for (
+      let last = 0, i = 0;
+      i < data.length;
+      last = last + data[i].ratio, ++i
+    ) {
+      const [sx, sy] = coord(last);
+      const [ex, ey] = coord(last + data[i].ratio);
+      const large = data[i].ratio > 0.5 ? 1 : 0;
+
+      paths.push({
+        label: data[i].label,
+        color: data[i].color,
+        d: `M ${sx} ${sy} A 1 1 0 ${large} 1 ${ex} ${ey} L 0 0`,
+      });
+    }
+
+    return paths;
+  }, [data]);
 
   return (
     <svg viewBox="-1 -1 2 2" style={{ transform: "rotate(-0.25turn)" }}>
-      {paths}
+      {paths.map(({ label, color, d }, index) => (
+        <path
+          key={index.toString()}
+          d={d}
+          fill={color}
+          fillOpacity={
+            typeof highlight !== "string" || highlight === label ? "1.0" : "0.1"
+          }
+          onMouseOver={() => {
+            if (onHighlight) {
+              onHighlight(label, index);
+            }
+          }}
+          onMouseOut={() => {
+            if (onHighlight) {
+              onHighlight(null, -1);
+            }
+          }}
+        />
+      ))}
     </svg>
   );
 };
