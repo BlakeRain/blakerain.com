e982d4ac1b8f1d29e1505868c786f3cc66983343
Blake Rain <blake.rain@blakerain.com>
2021-12-31 03:55:02 +0000

    Remove old search highlighter

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,13 +1,8 @@
 import React, { FC } from "react";
-import { useRouter } from "next/router";
 
 import { Root } from "mdast";
 import { DocInfo, Tag } from "../lib/content";
 
-import {
-  getHighlightTerm,
-  SearchHighlighter,
-} from "./search/SearchHighlighter";
 import { ScrollToTopButton } from "./ScrollToTop";
 import { PostDetails } from "./PostDetails";
 import { TagList } from "./TagList";
@@ -28,19 +23,16 @@ const ContentHeader: FC<{ tags?: Tag[]; doc: DocInfo }> = ({ tags, doc }) => {
   );
 };
 
-const ContentBody: FC<{ doc: DocInfo; root: Root }> = ({ doc, root }) => {
-  const router = useRouter();
-  const term = getHighlightTerm(router.query);
-
+const ContentBody: FC<{ root: Root }> = ({ root }) => {
   return (
-    <SearchHighlighter term={term}>
+    <React.Fragment>
       <div className={styles.content}>
         <div className={styles.contentInner}>
           <Render node={root} />
         </div>
       </div>
       <ScrollToTopButton />
-    </SearchHighlighter>
+    </React.Fragment>
   );
 };
 
@@ -54,7 +46,7 @@ export const Content: FC<ContentProps> = ({ tags, doc, root }) => {
   return (
     <article className="post">
       <ContentHeader tags={tags} doc={doc} />
-      <ContentBody doc={doc} root={root} />
+      <ContentBody root={root} />
     </article>
   );
 };
