89cf309a8c45e833110764ec19d7681a8264789b
Blake Rain <blake.rain@blakerain.com>
2022-01-04 13:48:52 +0000

    Improve scroll-to-top button position and animation

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,9 +1,11 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import cn from "classnames";
 import styles from "./ScrollToTop.module.scss";
 
 export const ScrollToTopButton: FC = () => {
+  const footerRef = useRef<HTMLElement | null>(null);
   const [visible, setVisible] = useState<boolean>(false);
+  const [footerVisible, setFooterVisible] = useState<boolean>(false);
 
   const onDocumentScroll = () => {
     if (window.pageYOffset > 300) {
@@ -13,22 +15,56 @@ export const ScrollToTopButton: FC = () => {
     }
   };
 
+  const onFooterIntersection: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setFooterVisible(true);
+      } else {
+        setFooterVisible(false);
+      }
+    });
+  };
+
   useEffect(() => {
     document.addEventListener("scroll", onDocumentScroll);
 
+    const footer = document.querySelector("footer");
+    let observer: IntersectionObserver | null = null;
+
+    if (footer) {
+      observer = new IntersectionObserver(onFooterIntersection);
+      observer.observe(footer);
+      footerRef.current = footer;
+    }
+
     return () => {
       document.removeEventListener("scroll", onDocumentScroll);
+      if (observer) {
+        observer.disconnect();
+      }
     };
   }, []);
 
-  return visible ? (
+  return (
     <button
-      className={cn(styles.button, "scroll-button")}
+      className={cn(styles.button, "scroll-button", {
+        [styles.visible]: visible,
+        [styles.skipFooter]: footerVisible,
+      })}
+      style={{
+        transform:
+          visible && footerVisible && footerRef.current
+            ? `translateY(-${
+                footerRef.current.getBoundingClientRect().height
+              }px)`
+            : undefined,
+      }}
       tabIndex={-1}
       onClick={() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
-      }}>
+      }}
+    >
       &uarr; Goto Top
     </button>
-  ) : null;
+  );
 };
