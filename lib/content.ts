bf68ee2a2241a9b9314fa8fb37e0bed5c85fbbe1
Blake Rain <blake.rain@blakerain.com>
2022-02-14 11:56:55 +0000

    Add links to headings

--- lib/content.ts
+++ lib/content.ts
@@ -10,6 +10,7 @@ import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { remarkMdxCodeMeta } from "remark-mdx-code-meta";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import matter from "gray-matter";
 import { IndexBuilder, PreparedIndex } from "./search";
 
@@ -153,7 +154,7 @@ async function loadDoc<P extends Preamble>(
       scope: preamble as Record<string, any>,
       mdxOptions: {
         remarkPlugins: [remarkPlugin, remarkMdxCodeMeta, remarkGfm],
-        rehypePlugins: [rehypePlugin, rehypeSlug],
+        rehypePlugins: [rehypePlugin, rehypeSlug, rehypeAutolinkHeadings],
       },
     }),
   };
