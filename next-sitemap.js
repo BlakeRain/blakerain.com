c7b0a81834a40cba06788a272fe0110e0ad19ef6
Blake Rain <blake.rain@blakerain.com>
2022-02-19 17:16:29 +0000

    Remove disclaimer from sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://blakerain.com",
   generateRobotsTxt: true,
-  exclude: ["/analytics"],
+  exclude: ["/analytics", "/disclaimer"],
 };
