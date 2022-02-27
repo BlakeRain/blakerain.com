34c597bbdb81cd2164aeee09aa3181e86e8a20d0
Blake Rain <blake.rain@blakerain.com>
2022-02-27 13:52:18 +0000

    Skip optimisation of images that have not changed

--- tools/optimize-images.js
+++ tools/optimize-images.js
@@ -1,6 +1,7 @@
 const fs = require("fs").promises;
 const sharp = require("sharp");
 const path = require("path");
+const { createHash } = require("crypto");
 
 const baseDir = path.join(process.cwd(), "public", "content");
 const outDir = path.join(process.cwd(), "public", "content");
@@ -8,6 +9,28 @@ const outDir = path.join(process.cwd(), "public", "content");
 const IMAGE_SIZES = [16, 32, 48, 64, 96, 128, 256, 384];
 const DEVICE_SIZES = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
 const QUALITY = 75;
+
+function generateHash(image) {
+  const hash = createHash("sha256");
+  hash.update(image);
+  return hash.digest("base64").replace(/\//g, "-");
+}
+
+const HASHES_PATH = path.join(outDir, "images.sha256.json");
+
+async function loadImageHashes() {
+  try {
+    await fs.access(HASHES_PATH, fs.R_OK);
+    const hashes_raw = await fs.readFile(HASHES_PATH, "utf-8");
+    return JSON.parse(hashes_raw);
+  } catch {
+    return {};
+  }
+}
+
+async function storeImageHashes(hashes) {
+  await fs.writeFile(HASHES_PATH, JSON.stringify(hashes));
+}
 
 function isImageExtension(extension) {
   return [".jpg", ".jpeg", ".webp", ".png", ".avif"].includes(extension);
@@ -48,14 +71,32 @@ async function main() {
     await fs.mkdir(opt_dir, { recursive: true });
   }
 
+  // Load the set of hashes
+  const image_hashes = await loadImageHashes();
+
   const widths = [...IMAGE_SIZES, ...DEVICE_SIZES];
-  let total = 0;
+  let total_bytes = 0;
+  let skipped = 0;
 
   console.log(
     `Processing ${info.images.length} image(s) over ${widths.length} size(s) ...`
   );
   for (let image of info.images) {
     const image_buf = await fs.readFile(path.join(baseDir, image.relative));
+    const image_hash = generateHash(image_buf);
+
+    // See if the image has changed from our previously recorded hash. If so,
+    // then we can assume that it has not changed and skip it.
+    const expected_hash = image_hashes[image.relative];
+    if (expected_hash) {
+      if (image_hash === expected_hash) {
+        ++skipped;
+        continue;
+      }
+    }
+
+    // Store the hash of the image
+    image_hashes[image.relative] = image_hash;
 
     for (let width_idx = 0; width_idx < widths.length; ++width_idx) {
       const width = widths[width_idx];
@@ -89,12 +130,22 @@ async function main() {
       }
 
       const info = await transformer.toFile(out_path);
-      total += info.size;
+      total_bytes += info.size;
     }
   }
 
+  // Store the image hashes
+  storeImageHashes(image_hashes);
+
   console.log(
-    `Generated ${(total / (1024 * 1024)).toFixed(2)} Mib of optimized images`
+    `Processed ${info.images.length - skipped} of ${
+      info.images.length
+    } image(s); skipped ${skipped} unchanged image(s)`
+  );
+  console.log(
+    `Generated ${(total_bytes / (1024 * 1024)).toFixed(
+      2
+    )} Mib of optimized images`
   );
 }
 
