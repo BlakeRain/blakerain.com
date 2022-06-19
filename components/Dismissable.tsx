be5cb22792410755a2fee0cbfd365f6cb464b116
Blake Rain <blake.rain@blakerain.com>
2022-06-19 12:26:01 +0100

    Tidy up the footer a bit more

--- components/Dismissable.tsx
+++ components/Dismissable.tsx
@@ -1,17 +1,23 @@
 import React, { FC, useEffect, useRef } from "react";
 
+/**
+ * Provides a dismissable wrapper around an element
+ *
+ * When a mouse click event is received outside of this component, the `onDismiss` property is called.
+ */
 export const Dismissable: FC<{
   onDismiss: (event: MouseEvent) => void;
   className?: string;
 }> = ({ onDismiss, className, children }) => {
-  const outerDiv = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
+      // Make sure that the click was not on or in our container before we call the `onDismiss` function.
       if (
         event.target &&
-        outerDiv.current &&
-        !outerDiv.current.contains(event.target as Node)
+        container.current &&
+        !container.current.contains(event.target as Node)
       ) {
         onDismiss(event);
       }
@@ -24,7 +30,7 @@ export const Dismissable: FC<{
   }, [onDismiss]);
 
   return (
-    <div ref={outerDiv} className={className}>
+    <div ref={container} className={className}>
       {children}
     </div>
   );
