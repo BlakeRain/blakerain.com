d14ebaf95b48673ed330c16ec239654ed5c956ab
Blake Rain <blake.rain@blakerain.com>
2023-01-27 11:56:40 +0000

    Sort ranges when received from search index

--- components/article/Render.tsx
+++ components/article/Render.tsx
@@ -64,7 +64,10 @@ const SearchPositionsProvider: FC<
     for (const position of results.get(docId) || []) {
       const location = index.locations.getLocation(position.location_id);
       if (location) {
-        loaded.push({ path: location.path, ranges: position.ranges });
+        loaded.push({
+          path: location.path,
+          ranges: position.ranges.sort((a, b) => a.start - b.start),
+        });
       }
     }
 
@@ -157,7 +160,7 @@ function renderHighlight(ranges: Range[], text: string): React.ReactElement {
   const parts: React.ReactElement[] = [];
   let start_index = 0;
 
-  for (const range of ranges.sort((a, b) => a.start - b.start)) {
+  for (const range of ranges) {
     const prefix = text.substring(start_index, range.start);
     if (prefix.length > 0) {
       parts.push(
