b781d50618a49cea463c3e156a551c1f0fa0d47f
Blake Rain <blake.rain@blakerain.com>
2024-09-13 17:14:13 +0100

    chore: tidy up background and photo images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
     extend: {
+      backgroundImage: {
+        "cartographer-light": "url('/media/cartographer-light.webp')",
+        "cartographer-dark": "url('/media/cartographer-dark.webp')",
+      },
       colors: {
         primary: {
           dark: "#22293D",
